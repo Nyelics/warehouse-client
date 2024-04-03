ab14c51bc771f8d5e06ef257e119142637e3e49d
Nyel <120316605+nyelandres@users.noreply.github.com>
2024-04-03 15:04:02 +0800

    remove manifest

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import {io} from "socket.io-client";
-const URL = "http://localhost:3001";
+const URL = "https://bcp-warehouse-ws.netlify.app/";
 
 export const socket = io(URL, {
   autoConnect: false,
