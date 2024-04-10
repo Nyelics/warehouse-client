412e215cb61162b78d8cff68c1512fd491dc01ba
Nyel <120316605+nyelandres@users.noreply.github.com>
2024-04-10 21:04:52 +0800

    toolkit remove

--- src/components/BSTableComponent.jsx
+++ src/components/BSTableComponent.jsx
@@ -1,5 +1,4 @@
 import BootstrapTable from "react-bootstrap-table-next";
-import ToolkitProvider, {CSVExport} from "react-bootstrap-table2-toolkit";
 import cellEditFactory from "react-bootstrap-table2-editor";
 import paginationFactory from "react-bootstrap-table2-paginator";
 import PropTypes from "prop-types"; // Import PropTypes
@@ -12,45 +11,24 @@ const BSTableComponent = ({
   BSRowEvent,
   BShandleCellEdit,
 }) => {
-  const {ExportCSVButton} = CSVExport;
   return (
     <>
-      <ToolkitProvider
-        keyField={BSKey}
-        data={BSData}
-        columns={BSColumn}
-        exportCSV
-      >
-        {(props) => (
-          <div>
-            <div className="d-flex">
-              <ExportCSVButton
-                {...props.csvProps}
-                className="btn btn-primary p-2"
-                style={{marginLeft: "auto"}}
-              >
-                Export Report
-              </ExportCSVButton>
-            </div>
-
-            <hr />
-            <BootstrapTable
-              keyField={BSKey}
-              data={BSData}
-              columns={BSColumn}
-              expandRow={BSexpandRow}
-              rowEvents={BSRowEvent}
-              pagination={paginationFactory()}
-              cellEdit={cellEditFactory({
-                mode: "click",
-                afterSaveCell: BShandleCellEdit,
-                blurToSave: true,
-              })}
-              {...props.baseProps}
-            />
-          </div>
-        )}
-      </ToolkitProvider>
+      <div>
+        <hr />
+        <BootstrapTable
+          keyField={BSKey}
+          data={BSData}
+          columns={BSColumn}
+          expandRow={BSexpandRow}
+          rowEvents={BSRowEvent}
+          pagination={paginationFactory()}
+          cellEdit={cellEditFactory({
+            mode: "click",
+            afterSaveCell: BShandleCellEdit,
+            blurToSave: true,
+          })}
+        />
+      </div>
     </>
   );
 };
