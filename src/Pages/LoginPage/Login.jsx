f78aa4a5eb9353bd006cedd8cb79e1174ca02ad9
Nyel <120316605+nyelandres@users.noreply.github.com>
2024-04-11 14:36:12 +0800

    Update Login.jsx

--- src/Pages/LoginPage/Login.jsx
+++ src/Pages/LoginPage/Login.jsx
@@ -14,10 +14,10 @@ const Login = () => {
 
   const routes = [
     {role_name: "Administrator", route: "/dashboard"},
-    // {role_name: "Warehouse Manager", route: "/wm/dashboard"},
-    // {role_name: "Dummy Supplier", route: "/ds/reports"},
-    // {role_name: "Quality Engineer", route: "/qe/dashboard"},
-    // {role_name: "Warehouse Associate", route: "/wa/inventory"},
+    {role_name: "Warehouse Manager", route: "/wm/dashboard"},
+    {role_name: "Dummy Supplier", route: "/ds/reports"},
+    {role_name: "Quality Engineer", route: "/qe/dashboard"},
+    {role_name: "Warehouse Associate", route: "/wa/inventory"},
   ];
 
   async function onSubmit(values) {
