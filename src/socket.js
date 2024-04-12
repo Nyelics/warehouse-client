bcaf5e798cd7625717ca4af69dcd0a3dfcbb97d7
Nyel <120316605+nyelandres@users.noreply.github.com>
2024-04-12 13:53:18 +0800

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import {io} from "socket.io-client";
-const WEBSOCKET_URL = import.meta.env.VITE_API_WS_URL;
-// const WEBSOCKET_URL = import.meta.env.VITE_API_LOCAL_WS_URL;
-console.log(WEBSOCKET_URL);
+// const WEBSOCKET_URL = import.meta.env.VITE_API_WS_URL;
+const WEBSOCKET_URL = import.meta.env.VITE_API_LOCAL_WS_URL;
+
 export const socket = io(WEBSOCKET_URL, {
   autoConnect: false,
 });
